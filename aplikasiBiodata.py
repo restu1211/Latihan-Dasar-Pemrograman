a14c1a34b4fe3bc5e04d0118dc0577bd09cfe052
Restu Ramdani_TI22A <114630690+restu1211@users.noreply.github.com>
2022-09-29 11:18:55 +0700

    Add files via upload

---
+++ aplikasiBiodata.py
@@ -0,0 +1,16 @@
+# aplikasi biodata create by restu
+
+# memasukan nama
+print ( " mohon lengkapi data diri anda !")
+
+#input Data Diri
+nama = input (" nama : ")
+kelas = input (" kelas : ")
+prodi = input (" prodi :  ")
+tempat_tanggal_lahir = input (" Tempat tanggal lahir : ")
+
+# print / output Data Diri
+print (" nama anda adalah : ", nama)
+print (" kelas anda adalah : ", kelas)
+print (" prodi anda adalah : ", prodi)
+print (" tempat tanggal lahir anda adalah : ", tempat_tanggal_lahir)
