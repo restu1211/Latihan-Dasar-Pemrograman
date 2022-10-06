8b7d177242875064054786e2f4b0a3b1af35810c
Restu Ramdani_TI22A <114630690+restu1211@users.noreply.github.com>
2022-10-06 09:21:37 +0700

    Restu Ramdani_TI22A

---
+++ tugas_sesi3.py
@@ -0,0 +1,13 @@
+# Restu Ramdani_TI22A
+
+print ( " Buatlah Biodata Sederhana ")
+Nama = " Restu Ramdani "
+Kelas = " TI22A "
+Prodi = " Teknik Informatika "
+Quote = " Jatuh bangun, jatuh bangun, lalu bangun tanpa menjatuhkan "
+
+# print 
+print (" nama      : ", Nama)
+print (" kelas     : ", Kelas)
+print (" prodi     : ", Prodi)
+print (" quote     : ", Quote)
